942a452ff0224d0280ad7cada3ba79440bec8240
ElenaBoyko1992 <Bionka@inbox.ru>
2022-08-08 19:33:03 +0300

    delete .idea

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <div>
             <a href='#s'>Home</a>
-            <a href='#s'>News Feed</a>
+            <a href='#s'>New Feed</a>
             <a href='#s'>Messages</a>
         </div>
     );
